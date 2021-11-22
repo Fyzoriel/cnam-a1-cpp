deedf78a014eac6be1073e58db61b117b4b4efe6
Allard-Benoit <91566255+Allard-Benoit@users.noreply.github.com>
2021-11-22 20:33:26 +0100

    Feat(ReversiGrid): Add return token function

--- TP5/Grids/Reversi/ReversiGrid.cpp
+++ TP5/Grids/Reversi/ReversiGrid.cpp
@@ -2,10 +2,10 @@
 // Created by boybe on 21/11/2021.
 //
 
-#include <Grids/Grid.h>
 #include <Utils/AskUser.h>
 #include "ReversiGrid.h"
 #include "Player.h"
+#include "vector"
 
 ReversiGrid::ReversiGrid() : Grid(4,7){}
 
@@ -13,6 +13,7 @@ bool ReversiGrid::checkToken(const Player &player, int x, int y)
 {
     int direction = 1;
     char color = player.getColor();
+    vector<int> goodDirection;
     while(direction < 9)
     {
         switch (direction)
@@ -31,7 +32,7 @@ bool ReversiGrid::checkToken(const Player &player, int x, int y)
                             if (m_grid[y - i][x].getColor() == ' ')
                                 direction++;
                             else if (m_grid[y - i][x].getColor() == color)
-                                return true;
+                                goodDirection.push_back(direction);
                         } while (m_grid[y - i][x].getColor() != color && m_grid[y - i][x].getColor() != ' ');
                     }
                 }
@@ -53,7 +54,7 @@ bool ReversiGrid::checkToken(const Player &player, int x, int y)
                             if (m_grid[y - i][x + i].getColor() == ' ')
                                 direction++;
                             else if (m_grid[y - i][x + i].getColor() == color)
-                                return true;
+                                goodDirection.push_back(direction);
                         } while (m_grid[y - i][x + i].getColor() != color && m_grid[y - i][x + i].getColor() != ' ');
                     }
                 }
@@ -75,7 +76,7 @@ bool ReversiGrid::checkToken(const Player &player, int x, int y)
                             if (m_grid[y][x + i].getColor() == ' ')
                                 direction++;
                             else if (m_grid[y][x + i].getColor() == color)
-                                return true;
+                                goodDirection.push_back(direction);
                         } while (m_grid[y][x + i].getColor() != color && m_grid[y][x + i].getColor() != ' ');
                     }
                 }
@@ -94,10 +95,10 @@ bool ReversiGrid::checkToken(const Player &player, int x, int y)
                         do
                         {
                             i++;
-                            if (Grid[y + i][x + i].getColor() == ' ')
+                            if (m_grid[y + i][x + i].getColor() == ' ')
                                 direction++;
-                            else if (Grid[y + i][x + i].getColor() == color)
-                                return true;
+                            else if (m_grid[y + i][x + i].getColor() == color)
+                                goodDirection.push_back(direction);
                         } while (m_grid[y + i][x + i].getColor() != color && m_grid[y + i][x + i].getColor() != ' ');
                     }
                 }
@@ -119,7 +120,7 @@ bool ReversiGrid::checkToken(const Player &player, int x, int y)
                             if (m_grid[y + i][x].getColor() == ' ')
                                 direction++;
                             else if (m_grid[y + i][x].getColor() == color)
-                                return true;
+                                goodDirection.push_back(direction);
                         } while (m_grid[y + i][x].getColor() != color && m_grid[y + i][x].getColor() != ' ');
                     }
                 }
@@ -141,7 +142,7 @@ bool ReversiGrid::checkToken(const Player &player, int x, int y)
                             if (m_grid[y + i][x - i].getColor() == ' ')
                                 direction++;
                             else if (m_grid[y + i][x - i].getColor() == color)
-                                return true;
+                                goodDirection.push_back(direction);
                         } while (m_grid[y + i][x - i].getColor() != color && m_grid[y + i][x - i].getColor() != ' ');
                     }
                 }
@@ -163,7 +164,7 @@ bool ReversiGrid::checkToken(const Player &player, int x, int y)
                             if (m_grid[y][x - i].getColor() == ' ')
                                 direction++;
                             else if (m_grid[y][x - i].getColor() == color)
-                                return true;
+                                goodDirection.push_back(direction);
                         } while (m_grid[y][x - i].getColor() != color && m_grid[y][x - i].getColor() != ' ');
                     }
                 }
@@ -185,14 +186,89 @@ bool ReversiGrid::checkToken(const Player &player, int x, int y)
                             if (m_grid[y - i][x - i].getColor() == ' ')
                                 direction++;
                             else if (m_grid[y - i][x - i].getColor() == color)
-                                return true;
+                                goodDirection.push_back(direction);
                         } while (m_grid[y - i][x - i].getColor() != color && m_grid[y - i][x - i].getColor() != ' ');
                     }
                 }
                 else
                     direction++;
-                break;
+
         }
+    }
+    if(goodDirection.empty())
+        return false;
+    else
+    {
+        int direc = 0;
+        for (int z = 0; z < goodDirection.size(); z++) { // pour retourner les jetons
+            direc = goodDirection[z];
+            switch (direc) {
+                case 1: // vers le haut
+                    int i = 1;
+                    while (m_grid[y - i][x].getColor() != color) {
+                        m_grid[y - i][x].addToken(player);
+                        i++;
+                    }
+                    break;
+
+                case 2: // diagonale haut droite
+                    int i = 1;
+                    while (m_grid[y - i][x + i].getColor() != color) {
+                        m_grid[y - i][x + i].addToken(player);
+                        i++;
+                    }
+                    break;
+
+                case 3: // vers la droite
+                    int i = 1;
+                    while (m_grid[y][x + i].getColor() != color) {
+                        m_grid[y][x + i].addToken(player);
+                        i++;
+                    }
+                    break;
+
+                case 4: // diagonale bas droite
+                    int i = 1;
+                    while (m_grid[y + i][x + i].getColor() != color) {
+                        m_grid[y + i][x + i].addToken(player);
+                        i++;
+                    }
+                    break;
+
+                case 5: // vers le bas
+                    int i = 1;
+                    while (m_grid[y + i][x].getColor() != color) {
+                        m_grid[y + i][x].addToken(player);
+                        i++;
+                    }
+                    break;
+
+                case 6: // diagonale bas gauche
+                    int i = 1;
+                    while (m_grid[y + i][x - i].getColor() != color) {
+                        m_grid[y + i][x - i].addToken(player);
+                        i++;
+                    }
+                    break;
+
+                case 7: // vers la droite
+                    int i = 1;
+                    while (m_grid[y][x - i].getColor() != color) {
+                        m_grid[y][x - i].addToken(player);
+                        i++;
+                    }
+                    break;
+
+                case 8: // diagonale haut gauche
+                    int i = 1;
+                    while (m_grid[y - i][x - i].getColor() != color) {
+                        m_grid[y - i][x - i].addToken(player);
+                        i++;
+                    }
+                    break;
+            }
+        }
+        return true;
     }
 }
 
